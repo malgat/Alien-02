6145a2d9689bbc08a3cbb6bc83e8e8c2740d0442
chanseok <miss3378@naver.com>
2019-03-28 14:21:21 +0900

    Initial Commit

---
+++ Ailien ImageView02Tests/Ailien_ImageView02Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Ailien_ImageView02Tests.swift
+//  Ailien ImageView02Tests
+//
+//  Created by D7703_21 on 2019. 3. 28..
+//  Copyright © 2019년 D7703_21. All rights reserved.
+//
+
+import XCTest
+@testable import Ailien_ImageView02
+
+class Ailien_ImageView02Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
